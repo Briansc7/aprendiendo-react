a4b162f03fca6c0d62fd92e24a138715639ce591
Briansc7 <samudio.utn@gmail.com>
2023-06-10 10:24:40 -0300

    removed counter reference on home jsx

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import Counter from "../../common/counter/Counter";
 
 const Home = () => {
   return (
